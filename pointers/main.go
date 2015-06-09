3ee0f6e990c2be9fc24039f8ec58efbfa203c659
Julio Lopez <ljuliom@gmail.com>
2015-06-08 21:27:52 -0500

    pointers example

---
+++ pointers/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type User struct{
+  name string
+}
+
+func change_without_pointer(user User){
+  user.name = "Piero"
+}
+
+func change_with_pointer(user *User){
+  user.name = "Piero"
+}
+
+func main() {
+  user := User{"Julio"}
+  fmt.Println("Say hello to the pointer!: ", &user)
+  fmt.Println(user)
+  change_without_pointer(user)
+  fmt.Println(user)
+  change_with_pointer(&user)
+  fmt.Println(user)
+}
